dce58e8a5816d31ddd3db348dfaa86516f8a7f35
ekoniuh <dr_zek@mail.ru>
2020-10-03 07:06:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  return  String(n > 0 ? n : -1 * n).split('').reverse().join('');
+};
+
+// module.exports = function reverse (n) {
+//     return parseInt(n.toString().split('').reverse().join(''));
+// }
